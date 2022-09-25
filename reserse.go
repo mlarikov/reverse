e054d645bf4ca50bd725639f6acf2834cef06ddd
Mikhail Larikov <mlarikov@gmail.com>
2022-09-25 13:54:54 +0300

    HZ commit

--- reserse.go
+++ reserse.go
@@ -1,12 +1,23 @@
 package main
 
-const width int = 8
-const heigth int = 8
+import "fmt"
 
-func getNewBoard() (board [width][heigth]string) {
+const fieldRow int = 8
+const fieldCol int = 8
 
+func getNewBoard() [fieldRow][fieldCol]string {
+	var board [fieldRow][fieldCol]string
+
+	for row := 0; row < len(board); row++ {
+		for col := 0; col < len(board[row]); col++ {
+			board[row][col] = " "
+		}
+	}
+
+	return board
 }
 
 func main() {
-
+	board := getNewBoard()
+	fmt.Println(board)
 }
