87234e33e09883d828b1fd6af49f736993914290
Mikhail Larikov <mlarikov@gmail.com>
2022-09-26 12:44:23 +0300

    Chanded init board way

--- reserse.go
+++ reserse.go
@@ -11,11 +11,16 @@ const fieldCol int = 8
 func getNewBoard() [fieldRow][fieldCol]string {
 	var board [fieldRow][fieldCol]string
 
-	for row := 0; row < len(board); row++ {
-		for col := 0; col < len(board[row]); col++ {
+	for row, rowVal := range board {
+		for col, _ := range rowVal {
 			board[row][col] = " "
 		}
 	}
+	// for row := 0; row < len(board); row++ {
+	// 	for col := 0; col < len(board[row]); col++ {
+	// 		board[row][col] = " "
+	// 	}
+	// }
 
 	return board
 }
