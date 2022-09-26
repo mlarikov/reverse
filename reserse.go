9d1a5a46423504ac4e901f3885165976935d8eee
Mikhail Larikov <mlarikov@gmail.com>
2022-09-26 12:44:23 +0300

    Chanded init board way

--- reserse.go
+++ reserse.go
@@ -11,27 +11,32 @@ const fieldCol int = 8
 func getNewBoard() [fieldRow][fieldCol]string {
 	var board [fieldRow][fieldCol]string
 
-	for row := 0; row < len(board); row++ {
-		for col := 0; col < len(board[row]); col++ {
-			board[row][col] = " "
+	for row, rowVal := range board {
+		for col, _ := range rowVal {
+			board[row][col] = "X"
 		}
 	}
+	// for row := 0; row < len(board); row++ {
+	// 	for col := 0; col < len(board[row]); col++ {
+	// 		board[row][col] = " "
+	// 	}
+	// }
 
 	return board
 }
 
 func drawBoard(board [fieldRow][fieldCol]string) {
-	fmt.Println("   1  2  3  4  5  6  7  8")
-	fmt.Println(" +------------------------+")
+	fmt.Println("   1 2 3 4 5 6 7 8")
+	fmt.Println(" +-----------------+")
 	for i, row := range board {
-		fmt.Printf("%v|", i+1)
+		fmt.Printf("%v| ", i+1)
 		for _, val := range row {
-			fmt.Print(" ", val, " ")
+			fmt.Printf("%v ", val)
 		}
 		fmt.Printf("|%v\n", i+1)
 	}
-	fmt.Println(" +------------------------+")
-	fmt.Println("   1  2  3  4  5  6  7  8")
+	fmt.Println(" +-----------------+")
+	fmt.Println("   1 2 3 4 5 6 7 8")
 }
 
 func playGame(tab [][8]string) {
