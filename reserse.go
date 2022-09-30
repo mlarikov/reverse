8cd7247e7ce00088def8575893fe923cdb005f44
Mikhail Larikov <mlarikov@gmail.com>
2022-09-30 16:51:43 +0300

    Added calcValidMoves func

--- reserse.go
+++ reserse.go
@@ -58,7 +58,8 @@ func getValidMoves(board *[fieldRow][fieldCol]string, tile string) [][2]int {
 	// var cellValue []string
 	for row, rowVal := range board {
 		for col, _ := range rowVal {
-			if isValidMove(board, tile, row, col) {
+			tilesToFlip := calcValidMove(*board, tile, row, col)
+			if tilesToFlip != nil {
 				validMoves = append(validMoves, [2]int{row, col})
 			}
 			// validMoves = append(validMoves, [2]int{row, col})
@@ -69,8 +70,52 @@ func getValidMoves(board *[fieldRow][fieldCol]string, tile string) [][2]int {
 	return validMoves
 }
 
-func isValidMove(string *[fieldRow][fieldCol]string, tile string, row, col int) bool {
-	return true
+func calcValidMove(board [fieldRow][fieldCol]string, tile string, startRow, startCol int) [][2]int {
+	var tilesToFlip [][2]int
+
+	if board[startRow][startCol] != " " || !isOnBoard(startRow, startCol) {
+		return tilesToFlip
+	}
+
+	var otherTile string
+	if tile == "X" {
+		otherTile = "O"
+	} else {
+		otherTile = "X"
+	}
+
+	directions := [][2]int{{0, 1}, {1, 1}, {1, 0}, {1, -1},
+		{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}}
+	for direction := range directions {
+		rowDir := directions[direction][0]
+		colDir := directions[direction][1]
+		row, col := startRow, startCol
+		row += rowDir //First step in the y direction
+		col += colDir //First step in the x direction
+		for isOnBoard(row, col) && board[row][col] == otherTile {
+			// Keep moving in this direction.
+			row += rowDir
+			col += colDir
+			if isOnBoard(row, col) && board[row][col] == tile {
+				// There are pieces to flip over. Go in the reverse
+				// direction until we reach the original space,
+				// noting all the tiles along the way.
+				for {
+					row -= rowDir
+					col -= colDir
+					if row == startRow && col == startCol {
+						break
+					}
+					tilesToFlip = append(tilesToFlip, [2]int{row, col})
+				}
+			}
+		}
+	}
+	return tilesToFlip
+}
+
+func isOnBoard(row, col int) bool {
+	return row <= fieldRow-1 && row >= 0 && col >= 0 && col <= fieldCol-1
 }
 
 // Determine the score by counting the tiles. Return a dictionary
